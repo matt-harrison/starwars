b164fe085c30fde122733ec0e137d686efeb8a1f
Matt Harrison <matthewdh@gmail.com>
2024-08-03 15:20:24 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
